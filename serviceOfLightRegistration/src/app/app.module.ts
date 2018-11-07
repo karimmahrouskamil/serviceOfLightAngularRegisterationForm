dc50b3b7ef85570947966de05dd73fb1987d0eaf
karim kamil <kkamil@eg.ibm.com>
2018-11-07 11:19:42 +0200

    add bootstrap and scss format

--- serviceOfLightRegistration/src/app/app.module.ts
+++ serviceOfLightRegistration/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { RegistrationComponent } from './registration/registration.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
-  imports: [
+  imports: [NgbModule,
     BrowserModule
   ],
   providers: [],
