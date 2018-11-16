c1ba137d979882cd3170bc4894607330053d1207
karim kamil <kkamil@eg.ibm.com>
2018-11-16 18:45:11 +0200

    all form is ready

--- serviceOfLightRegistration/src/app/app.component.ts
+++ serviceOfLightRegistration/src/app/app.component.ts
@@ -33,6 +33,7 @@ this.makhdouminForm = new FormGroup({
       motherPhone : new FormControl('')
     }),
     school: new FormControl(''),
+    birthDate: new FormControl(''),
     onlineInfo: new FormGroup({
       facebook : new FormControl(''),
       email : new FormControl('')
@@ -47,7 +48,10 @@ this.makhdouminForm = new FormGroup({
   })
 });
 }
-
+fromJsonDate(jDate): Date {
+  const bDate: Date = new Date(jDate);
+  return bDate ;
+}
 // tslint:disable-next-line:no-trailing-whitespace
 onSubmit() {
 console.log(this.makhdouminForm.value);
@@ -58,3 +62,39 @@ this.makhdoumServ.createNewMakhdoum(this.makhdouminForm.value).subscribe(res => 
 });
 }
 }
+
+  // const vovo = {
+
+  //   'personalInfo': {
+  //     'name': 'margo',
+  //     'address': {
+  //       'buildingNumber': 5,
+  //       'streetName': 'Lololoalaoao',
+  //       'floorNumber': 32,
+  //       'flatNumber': 12,
+  //       'zone': 'B',
+  //       'remarks': 'left'
+  //     },
+  //     'Mobiles': {
+  //       'MobilePhone': [123123, 432403284],
+  //       'HomePhone': [5345345, 56546],
+  //       'fatherPhone': [23424, 654623],
+  //       'motherPhone': [234234, 54545]
+  //     },
+  //     'school': 'de la salle',
+  //     'birthDate': '2017-08-24T00:00:00.000Z',
+  //     'onlineInfo': {
+  //       'facebook': 'karim ',
+  //       'email': 'kkamil@eg'
+  //     }
+  //   },
+  //   'churchAndSpiritualInfo': {
+  //     'class': 1,
+  //     'fatherOfConfession': 'father youssef',
+  //     'servantFollowing': 'karim kamil',
+  //     'chomosya': 'esbaltos',
+  //     'Notes': 'quick note'
+  //   }
+  //   ,
+  //   'DateOfCreation': '2017-08-24T00:00:00.000Z'
+  // }
